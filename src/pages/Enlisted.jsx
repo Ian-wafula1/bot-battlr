d21b94fa453f7c7de3411d3e7bcd291d448b5336
Ian Wafula <neocorpse@proton.me>
2025-04-21 10:38:17 +0300

    Add jsx for Enlisted component

--- src/pages/Enlisted.jsx
+++ src/pages/Enlisted.jsx
@@ -1,7 +1,28 @@
+import Bot from "../components/Bot"
+import SortBar from "../components/SortBar"
+import useFetch from "../hooks/useFetch"
+import Filter from "../components/Filter"
+
 export default function Enlisted() {
-    return (
-        <main>
-            Enlisted
-        </main>
-    )
+    const {data: bots, loading, error} = useFetch('http://localhost:8001/enlisted')
+    if (loading) {
+        return <div className="loading">Loading...</div>
+    }
+    if (error) {
+        return <div className="error">Fetch error!</div>
+    }
+	return (
+		<main>
+			<h1>Enlisted</h1>
+			<div className="utilities">
+				<SortBar />
+				<Filter />
+			</div>
+			<div className="bots">
+				{bots.map((bot, index) => {
+					return <Bot index={index + 1} key={bot.id} bot={bot} />;
+				})}
+			</div>
+		</main>
+	);
 }
